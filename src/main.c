78cdb8cc229f26c0dbc51339c2b9b349337341c7
Cameron Lambert <calamber@e1z2r8p17.42.us.org>
2019-05-05 22:54:43 -0700

    fixed some stuff

--- src/main.c
+++ src/main.c
@@ -1,195 +1,155 @@
 #include "../includes/ft_ls.h"
-
-
-
-t_node *lst_swap(t_node *p1, t_node *p2)
+void  addtodirs(t_node **dirlist, t_node *files)
 {
-  p1->next = p2->next;
-  p2->next = p1;
-  return (p2);
+	t_node *new;
+	if(!(new = ft_memalloc(sizeof(t_node))))
+		return;
+	new->name = ft_strdup(files->name);
+	new->path = ft_strdup(files->path);
+	new->fullname = ft_strdup(files->fullname);
+	lstat(new->fullname, &(new->data));
+	new->isdir = (S_ISDIR(new->data.st_mode)) ? true : false;
+	new->next = (*dirlist)->next;
+	(*dirlist)->next = new;
+	*dirlist = new;
 }
 
-t_node *lst_sort_ascii(t_node *head)
+void  printit(t_node **dirlist, t_node *files, char flags)
 {
-
-  if (!head)
-    return (NULL);
-  if (head->next && ft_strcmp(head->path, head->next->path) > 0)
-    head = lst_swap(head, head->next);
-  head->next = lst_sort_ascii(head->next);
-  if (head->next && ft_strcmp(head->path, head->next->path) > 0)
-    {
-      head = lst_swap(head, head->next);
-      head->next = lst_sort_ascii(head->next);
-    }
-  return (head);
+	printf("%s:\n", files->path);
+	while (files)
+	{
+		if (!(flags & 0x10) && (files->name)[0] == '.')
+		{
+			files = files->next;
+			continue ;
+		}
+		if ((flags & 0x2) && files->isdir == true && ft_strcmp(files->name, ".") != 0 && ft_strcmp(files->name, "..") != 0)
+			addtodirs(dirlist, files);
+		printf("%s\n", files->name);
+		files = files->next;
+	}
+	printf("\n");
 }
 
 char          *makepath(char *s1, char *s2)
 {
-  char *ret;
-  char  *str;
-  if (!s2)
-    return (0);
-  if (!(str = ft_memalloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 2)))
-    return (0);
-  ret = str;
-  while (s1 && *s1)
-    *(str++) = *(s1++);
-  if (s1 && *(s1 - 1) != '/')
-      *(str++) = '/';
-  while (s2 && *s2)
-      *(str++) = *(s2++);
-    return (ret);
+	char *ret;
+	char  *str;
+
+	if (!s2)
+		return (0);
+	if (!(str = ft_memalloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 2)))
+		return (0);
+	ret = str;
+	while (s1 && *s1)
+		*(str++) = *(s1++);
+	if (s1 && *(s1 - 1) != '/')
+		*(str++) = '/';
+	while (s2 && *s2)
+		*(str++) = *(s2++);
+	return (ret);
 }
 
 void            addnode(t_node **head, char *path, char *name)
 {
-  t_node *new;
+	t_node *new;
 
-  if(!(new = ft_memalloc(sizeof(t_node))))
-    return;
-  
-new->name = ft_strdup(name);
-new->path = ft_strdup(path);
-new->fullname = makepath(path, name);
-lstat(new->fullname, &(new->data));
-new->isdir = (S_ISDIR(new->data.st_mode)) ? true : false;
-new->next = *head;
-*head = new;
-}
-
-int getargs(char *av,char *flags)
-{
-        if(*(av + 1) == '-')
-            return(0);
-    while(*(++av))
-    {
-
-        if(*av == 'r')
-        *flags |= 0x1;
-        else if(*av == 'R')
-        *flags |= 0x2;
-        else if(*av == 'l')
-        *flags |= 0x4;
-        else if(*av == 't')
-        *flags |= 0x8;
-        else if(*av == 'a')
-        *flags |= 0x10;
-    else
-    return(-1);
-    }
-    return(1);
-}
-
-char parse(char **av, t_node **dirlist)
-{
-char flags;
-int ret;
-ret = 0;
-flags = 0;
-av++;
-while(*av && **av == '-')
-{
-    if((ret = getargs((*av), &flags)) < 1)
-        break;
-    av++;
-  }
-  if (ret == -1)
-    return (0);
-  if (ret != -1 && !(*av))
-    addnode(dirlist, 0, ".");
-  while(*av)
-      {
-        addnode(dirlist, 0, *av);
-        av++;
-      }
-  dirlist = mergesort_list(dirlist, flags);
-  return(flags);
-}
-
-void  addtodirs(t_node **dirlist, t_node *fil)
-{
-t_node *new;
-  if(!(new = ft_memalloc(sizeof(t_node))))
-    return;
-new->name = ft_strdup(fil->name);
-new->path = ft_strdup(fil->path);
-new->fullname = ft_strdup(fil->fullname);
-lstat(new->fullname, &(new->data));
-new->isdir = (S_ISDIR(new->data.st_mode)) ? true : false;
-new->next = (*dirlist)->next;
-(*dirlist)->next = new;
-*dirlist = new;
-}
-
-void  printit(t_node **dirlist, t_node *fil, char flags)
-{
-  printf("%s:\n", fil->path);
-  while (fil)
-   {
-     if (!(flags & 0x10) && (ft_strcmp(fil->name, ".") == 0 || ft_strcmp(fil->name, "..") == 0))
-     {
-       fil = fil->next;
-       continue ;
-     }
-     if ((flags & 0x2) && fil->isdir == true && ft_strcmp(fil->name, ".") != 0 && ft_strcmp(fil->name, "..") != 0)
-          addtodirs(dirlist, fil);
-     printf("%s\n", fil->name);
-     fil = fil->next;
-   }
-   printf("\n");
+	if(!(new = ft_memalloc(sizeof(t_node))))
+		return;
+	new->name = ft_strdup(name);
+	new->path = ft_strdup(path);
+	new->fullname = makepath(path, name);
+	lstat(new->fullname, &(new->data));
+	new->isdir = (S_ISDIR(new->data.st_mode)) ? true : false;
+	new->next = *head;
+	*head = new;
 }
 
 void  useadir(t_node **dirlist, char flags)
 {
-    DIR *dir;
-  t_dirant *dp;
-  t_node *fil;
-  t_node *originaldir;
-  fil = 0;
-  originaldir = *dirlist;
-  //printf("opening: %s\n", (*dirlist)->fullname);
-  if (!(dir = opendir((*dirlist)->fullname)))
-    return ;
-while((dp = readdir (dir)) != NULL)
-   {
-     addnode(&fil, (*dirlist)->fullname,dp->d_name);
-   }
-   fil = *(mergesort_list(&fil, flags));
-   printit(dirlist, fil, flags);
-   closedir(dir);
-   *dirlist = originaldir;
+	DIR *dir;
+	t_dirant *dp;
+	t_node *files;
+	t_node *originaldir;
+	files = 0;
+	originaldir = *dirlist;
+	if (!(dir = opendir((*dirlist)->fullname)))
+		return ;
+	while((dp = readdir (dir)) != NULL)
+		addnode(&files, (*dirlist)->fullname, dp->d_name);
+	files = *(mergesort_list(&files, flags));
+	printit(dirlist, files, flags);
+	closedir(dir);
+	*dirlist = originaldir;
 }
 
 void usedirs(t_node **dirlist, char flags)
 {
-  t_node *top;
-  top = *dirlist;
-  while (*dirlist)
-  {
-    useadir(dirlist, flags);
-    *dirlist = (*dirlist)->next;
-  }
+	t_node *top;
+	top = *dirlist;
+	while (*dirlist)
+	{
+		useadir(dirlist, flags);
+		*dirlist = (*dirlist)->next;
+	}
+}
+
+int getargs(char *av,char *flags)
+{
+	if(*(av + 1) == '-')
+		return(0);
+	while(*(++av))
+	{
+		if(*av == 'r')
+			*flags |= 0x1;
+		else if(*av == 'R')
+			*flags |= 0x2;
+		else if(*av == 'l')
+			*flags |= 0x4;
+		else if(*av == 't')
+			*flags |= 0x8;
+		else if(*av == 'a')
+			*flags |= 0x10;
+		else
+			return(-1);
+	}
+	return(1);
+}
+
+char parse(char **av, t_node **dirlist)
+{
+	char flags;
+	int ret;
+	ret = 0;
+	flags = 0;
+	av++;
+	while(*av && **av == '-')
+	{
+		if((ret = getargs((*av), &flags)) < 1)
+				break;
+		av++;
+	}
+	if (ret == -1)
+		return (0);
+	if (!(*av))
+		addnode(dirlist, 0, ".");
+	while(*av)
+	{
+		addnode(dirlist, 0, *av);
+		av++;
+	}
+	dirlist = mergesort_list(dirlist, flags);
+	return(flags);
 }
 
 int main (int ac, char **av)
 {
-  char flags;
-  t_node *dirlist;
-  //t_node *originallist;
-  dirlist = 0;
-if((flags = parse(av,&dirlist)) == -1 && ac)
-  return(0);
-usedirs(&dirlist, flags);
- //sortlist();
- // print_ls_yo(fil);
- //closedir(dir);
- //makeldirs(somedirs);
- // sortlist(somelist);
-/*printls(sorted list);
-freelist(sorted list);
-*/
-
-return(0);
+	char flags;
+	t_node *dirlist;
+	dirlist = 0;
+	if((flags = parse(av,&dirlist)) == -1 && ac)
+		return(0);
+	usedirs(&dirlist, flags);
+	return(0);
 }
